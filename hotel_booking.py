72413a3d97f256210f2072f9882f0be9ba03679b
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-10-19 23:21:11 +0300

    Completion on the room class with methods and return

--- hotel_booking.py
+++ hotel_booking.py
@@ -6,4 +6,28 @@ class Room:
         self.room_number=room_number
         self.package_type=package_type
         self.price=price
-        
+        self.is_available =True
+        self.check_in_date = None  #default is none until set
+        self.check_out_date = None #default is none until set
+
+#methods involved with booking a room
+#method checking for room availability
+    def check_availability(self):
+        return self.is_available
+
+    def book_room(self,check_in_date,check_out_date):
+        self.is_available= False
+        self.check_in_date = check_in_date
+        self.check_out_date = check_out_date
+#method marking the room as available
+    def release_room(self):
+        self.is_available=True
+        self.check_in_date=None
+        self.check_out_date=None
+    
+    def __str__(self):
+        availability = "Available" if self.is_available else "Not Available"
+        return (f"Room {self.room_number}: {self.room_type}, Package: {self.package_type}, "
+                f"Price: ${self.price}, Check-in: {self.check_in_date}, "
+                f"Check-out: {self.check_out_date}, Status: {availability}")
+    
